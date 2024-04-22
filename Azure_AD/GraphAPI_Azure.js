9b4edea1fa7581f424e8455e4b09587d23ab0c6e
Amanda Guthrie <51721024+amandaguthrie@users.noreply.github.com>
2024-04-22 10:41:52 -0600

    Azure AD SAML Groups script usability improvements.

--- Azure_AD/GraphAPI_Azure.js
+++ Azure_AD/GraphAPI_Azure.js
@@ -1,103 +1,258 @@
-/*
-Microsoft identity platform and the OAuth 2.0 client credentials flow
-https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow
- 1. Get Tenant ID and User ID from Claims
- 2. Call MS Login to get Authorization token
- 3. Add Authorization token as Bearer JWT
- 4. Call MS Graph API to get groups that user is a member of
- 5. If a '"@odata.nextLink' is provided, then call again until all groups are loaded
- 6. Collect the displayNames of the Groups
-*/
+/**
+ * @name Appgate SAML Add >= 150 Groups to User Claims
+ * @description
+ * 1. Retrieves Tenant ID and User ID from user claims.
+ * 2. Requests an authorization token from Microsoft API and adds authorization token to headers.
+ * 3. Requests all groups a user is a member of from the Microsoft Graph API.
+ * 4. Reads paginated response(s) and aggregates group display names.
+ * 5. Returns the aggregated group display names as `azureGroups` in the claim.
+ * @returns {{azureGroups: string[], groupCount: number}}
+ * @ref https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow
+ */
 
-// update values below
+/**
+ * ====================================
+ *  Configurable Options
+ * ====================================
+ */
 
+/**
+ * @const applicationId
+ * @description The Azure registered application ID for Appgate which includes permissions to call the Microsoft Graph API
+ * @type {string}
+ * @example "9697a0f1-3b26-40a1-96c3-8d78da688c55"
+ */
 const applicationId = "... enter the application id here ...";
+
+/**
+ * @const clientSecret
+ * @description The corresponding client secret for the Azure registered application ID
+ * @type {string}
+ * @example "abc7Q~defghijklmnopqrs0t123456789-_.~"
+ */
 const clientSecret = "... enter the secret here ...";
 
+/**
+ * @const logging
+ * @description Whether claims script logging is enabled for troubleshooting.
+ * Set to `false` for production use.
+ * @type {boolean}
+ * @default `false`
+ */
 const logging = false;
 
 
-// advanced settings, contact Appgate support
+/**
+ * ====================================
+ * Advanced Options
+ * Contact Appgate support before changing anything below this line
+ * ====================================
+ */
 
-// group name prefix to filter, if any.
-const displayNameStartsWith = ""; 
+/**
+ * @const displayNamePrefixFilter
+ * @description Filter returned group names to only those which start with this string.
+ * @type {string}
+ */
+const displayNamePrefixFilter = "";
 
-const apiTimeOut = 1500; // miliseconds. max 3000 (default)
-const apiMaxPages = 5;   // max number of pages to follow in graph api
-const apiPageSize = 999;  // max number of groups per page. must be 999.
+/**
+ * @const apiTimeOut
+ * @description Requests to the MS Graph API time out after this many milliseconds.
+ * Default and maximum allowed is 3000ms.
+ * @type {number}
+ */
+const apiTimeOut = 1500;
 
-const cacheTokenHours = 0.5; // 0 to disable auth caching.
-const cacheGroupsHours = 0; // 0 to disable group caching. memory intensive.
+/**
+ * @const apiMaxPages
+ * @description The maximum number of pages to follow in the MS Graph API.
+ * @type {number}
+ */
+const apiMaxPages = 5;
 
-// do not edit any setting after this line
+/** @const apiPageSize
+ * @description The maximum number of groups returned per page. This must remain 999.
+ * @type {number}
+ * @default `999`
+ */
+const apiPageSize = 999;
 
-const apiUrl = `https://graph.microsoft.com/v1.0/users/${claims.user["UserId"]}/transitiveMemberOf`;
-const cacheKeyToken = `azure-ad-token-${claims.user["TenantId"]}`;
-const cacheKeyGroups = `azure-ad-groups-${claims.user["UserId"]}`;
+/**
+ * @const cacheTokenHours
+ * @description The amount of time in hours to cache the authentication token.
+ * Change to 0 to disable token caching.
+ * @type {number}
+ * @default 0.5
+ */
+const cacheTokenHours = 0.5;
 
+/**
+ * @const cacheGroupHours
+ * @description **Warning: Group caching is memory intensive.** The amount of time in hours to cache groups.
+ * `0` disables group caching.
+ * @type {number}
+ * @default 0
+ */
+const cacheGroupsHours = 0;
+
+/**
+ * ====================================
+ * Do not edit anything below this line
+ * ====================================
+ */
+
+/**
+ * @const apiUrl
+ * @description The Microsoft Graph API endpoint to get groups for a user.
+ * Uses the `UserId` claim to fill the user's object ID or user principal name.
+ * @type {string}
+ */
+const apiUrl = `https://graph.microsoft.com/v1.0/users/${ claims.user["UserId"] }/transitiveMemberOf`;
+/**
+ * @const cacheKeyToken
+ * @description The authentication token key name to store the authorization token in Appgate's cache.
+ * Irrelevant if @link cacheTokenHours is `0`
+ * @type {string}
+ */
+const cacheKeyToken = `azure-ad-token-${ claims.user["TenantId"] }`;
+/**
+ * @const cacheKeyGroups
+ * @type string
+ * @description The group cache key name to store groups in Appgate's cache.
+ * Irrelevant if @link cacheGroupsHours is `0`
+ * @type {string}
+ */
+const cacheKeyGroups = `azure-ad-groups-${ claims.user["UserId"] }`;
+
+/**
+ * @const headers
+ * @description The headers to include with MS Graph API requests.
+ * @type {{key: string, value: string}}[]
+ */
 const headers = [{ key: "Accept", value: "*/*" }];
-const empty = { "azureGroups": [], "groupCount": 0 };
 
-// fail if not configured correctly
-if (!claims.user["TenantId"] || !claims.user["UserId"] || !applicationId || !clientSecret) {
-  return empty;
-}
+/**
+ * @const defaultClaim
+ * @description The default claim returned if there is an error or no groups were found.
+ * @type {{groupCount: number, azureGroups: string[]}}
+ */
+const defaultClaim = { "azureGroups": [], "groupCount": 0 };
 
+
+/**
+ * @const log
+ * @description Helper function that logs a message to console when logging is enabled.
+ * @param {string} msg
+ */
 const log = (msg) => {
-  if (logging) {
+  if(logging) {
     console.log(msg);
   }
 }
 
-const processResponse = (response) => {
-  if (!response) {
-    log("No reponse from api request.");
+/**
+ * @const validateConfig
+ * @description Helper function that verifies configuration options are provided.
+ * @returns {boolean} `true` if all options are configured. `false` if one or more options are not configured.
+ */
+const validateConfig = () => {
+  let msg = "";
+  if(!claims.user["TenantId"]) {
+    msg += "TenantId map is not configured or is invalid."
+  }
+  if(!claims.user["UserId"]) {
+    msg += "UserId map is not configured or is invalid."
+  }
+  if(!applicationId) {
+    msg += "applicationId script variable is not configured or is invalid."
+  }
+  if(!clientSecret) {
+    msg += "clientSecret script variable is not configured or is invalid."
+  }
+  if(msg !== "") {
+    log(msg)
     return false;
   }
-  if (!response.data || response.statusCode != 200) {
-    log(`Request failed with code ${response.statusCode}`);
+  return true;
+}
+
+
+/**
+ * @const processResponse
+ * @description Helper function which processes an HTTP response and returns data or `false`.
+ * @param {{data: any, statusCode: number}} response The HTTP response to parse.
+ * @returns {{data: any}|boolean} When response data is present and OK status, returns parsed JSON data, otherwise returns false.
+ */
+const processResponse = (response) => {
+  if(!response) {
+    log("processResponse: No response received.");
+    return false;
+  }
+  if(!response.data || response.statusCode != 200) {
+    log(`processResponse: Request failed with code ${ response.statusCode }`);
     return false;
   }
   return JSON.parse(response.data);
 }
 
+/**
+ * @const getAccessToken
+ * @description Requests a token from Microsoft Graph API for the default scope.
+ * @returns {string|boolean} When successful, returns the access token, otherwise returns false.
+ */
 const getAccessToken = () => {
   const scope = "https://graph.microsoft.com/.default";
-  const payload = `grant_type=client_credentials&client_id=${applicationId}&client_secret=${clientSecret}&scope=${encodeURI(scope)}`;
-  const endpoint = `https://login.microsoftonline.com/${claims.user["TenantId"]}/oauth2/v2.0/token`;
+  const payload = `grant_type=client_credentials&client_id=${ applicationId }&client_secret=${ clientSecret }&scope=${ encodeURI(scope) }`;
+  const endpoint = `https://login.microsoftonline.com/${ claims.user["TenantId"] }/oauth2/v2.0/token`;
 
-  log("Sending token request.");
+  log("getAccessToken: Sending token request.");
   const response = httpPost(endpoint, payload, "application/x-www-form-urlencoded", [], apiTimeOut);
 
   const responseData = processResponse(response);
-  if (!responseData) {
+  if(!responseData) {
+    log("getAccessToken: Error obtaining access token.");
     return false;
   }
 
+  log("getAccessToken: Access token obtained.");
   return responseData.access_token;
 }
 
+/**
+ * @const getEndpointUrl
+ * @description Helper function which constructs the Microsoft Graph API endpoint URL with headers and query parameters.
+ * @returns {string} The constructed endpoint URL.
+ */
 const getEndpointUrl = () => {
-  const endpoint = `${apiUrl}?$top=${apiPageSize}`;
+  const endpoint = `${ apiUrl }?$top=${ apiPageSize }`;
 
-  if (!displayNameStartsWith) {
+  if(!displayNamePrefixFilter) {
+    log("getEndpointUrl: No display name filter configured; using default endpoint.")
     return endpoint;
   }
 
+  log("getEndpointUrl: Using endpoint with displayNamePrefixFilter")
   headers.push({ key: "ConsistencyLevel", value: "eventual" });
-  const filter = `&$count=true&$orderby=displayName&$filter=startswith(displayName,'${displayNameStartsWith}')`;
-  return `${endpoint}${encodeURI(filter)}`;
+  const filter = `&$count=true&$orderby=displayName&$filter=startswith(displayName,'${ displayNamePrefixFilter }')`;
+  return `${ endpoint }${ encodeURI(filter) }`;
 }
 
+/**
+ * @const getGroups
+ * @description Helper function which attempts to retrieve groups for a user. When more than one page is returned, handles pagination.
+ * @returns {string[]} Returns an array of group display names, or empty array if there is a failure.
+ */
 const getGroups = () => {
   const groups = [];
   const endpoint = getEndpointUrl();
 
-  log("Sending first groups request.");
+  log("getGroups: Sending Page 1 groups request.");
   const response = httpGet(endpoint, headers, apiTimeOut);
   const graphResultData = processResponse(response);
 
-  if (!graphResultData) {
+  if(!graphResultData) {
+    log("getGroups: graphResultData was invalid. Returning empty groups.")
     return groups;
   }
 
@@ -105,16 +260,16 @@ const getGroups = () => {
   let nextLink = graphResultData["@odata.nextLink"];
 
   let i = 0;
-  while (nextLink && i++ < apiMaxPages) {
+  while(nextLink && i++ < apiMaxPages) {
     const from = nextLink.indexOf("?");
     const until = nextLink.length;
     const parameterLink = nextLink.slice(from, until);
-    const newLink = `${apiUrl}${parameterLink}`;
+    const newLink = `${ apiUrl }${ parameterLink }`;
 
-    log(`Sending next groups request ${i + 1}.`);
+    log(`getGroups: Sending Page ${ i + 1 } groups request.`);
     const responseNext = httpGet(newLink, headers, apiTimeOut);
     const nextResultData = processResponse(responseNext);
-    if (nextResultData) {
+    if(nextResultData) {
       nextResultData["value"].forEach(group => groups.push(group["displayName"]));
       nextLink = nextResultData["@odata.nextLink"];
     } else {
@@ -125,10 +280,27 @@ const getGroups = () => {
   return groups;
 }
 
+/**
+ * ==========
+ * Main Module
+ * ==========
+ */
+
+if(!validateConfig()) {
+  return defaultClaim;
+}
+
+
+/**
+ * @name groups
+ * @type {string[]}
+ */
 let groups = [];
-if (cacheGroupsHours > 0) {
+
+/** Get groups from Appgate cache if group caching is configured */
+if(cacheGroupsHours > 0) {
   groups = cache.getIfPresent(cacheKeyGroups);
-  if (groups) {
+  if(groups) {
     log("Group caching is enabled, using the cache.");
     return { "azureGroups": groups, "groupCount": groups.length };
   } else {
@@ -136,34 +308,40 @@ if (cacheGroupsHours > 0) {
   }
 }
 
+/**
+ * @name token
+ * @type {string|boolean}
+ */
 let token;
-if (cacheTokenHours > 0) {
+
+/** Get token from Appgate cache if token caching is configured */
+if(cacheTokenHours > 0) {
   token = cache.getIfPresent(cacheKeyToken);
   log("Token caching is enabled, using the cache.");
 }
 
-if (!token) {
-  log("Token is empty, fetching again.");
+if(!token) {
+  log("Token cache is empty, fetching access token.");
   token = getAccessToken();
 
-  if (!token) {
-    log("Failed to fetch token, failing.");
-    return empty;
+  if(!token) {
+    log("Failed to fetch token from cache and Microsoft login API.");
+    return defaultClaim;
   }
 
-  if (cacheTokenHours > 0) {
+  if(cacheTokenHours > 0) {
     log("Token caching is enabled, caching the token.");
     cache.put(cacheKeyToken, token, 60 * 60 * cacheTokenHours);
   }
 }
 
-headers.push({ key: "Authorization", value: `Bearer ${token}` });
+headers.push({ key: "Authorization", value: `Bearer ${ token }` });
 
 groups = getGroups();
 
-log(`Fetched groups: ${groups.length}`);
+log(`Fetched ${ groups.length } groups.`);
 
-if (cacheGroupsHours > 0) {
+if(cacheGroupsHours > 0) {
   log("Group caching is enabled, caching the groups.");
   cache.put(cacheKeyGroups, groups, 60 * 60 * cacheGroupsHours);
 }
--- Azure_AD/README.md
+++ Azure_AD/README.md
@@ -1,70 +1,129 @@
-# Authorization for Azure AD  via MS Graph API
+# GraphAPI_Azure.js
 
-Using SAML and Graph API to authorize by Azure AD groups
+| On This Page |
+| --- |
+| [Objective](#objective)<br>[Prerequisites](#prerequisites)<br>[Configuration](#configuration) |
 
-## Scenario
+Using Azure AD SAML and Graph API to authorize by Azure AD group names
 
-A company using Azure Active Directory that has more than 150 user groups for any users and is using SAML for authentication.  There is a size restriction in the SAML assertion which causes no groups to be returned through the normal SAML assertion.
+## Objective
 
-The implementation comes from [Microsoft identity platform and the OAuth 2.0 client credentials flow](https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow) article.
+Allow companies using Azure Active Directory with SAML with more than 150 user groups to circumvent the 150 group limit.
 
->The OAuth 2.0 client credentials grant flow permits a web service (confidential client) to use its own credentials, instead of impersonating a user, to authenticate when calling another web service.
+For users with >= 150 group assignments, the SAML assertion contains a MS Graph API link to request the groups rather
+than a group list.
 
-### Prerequisites
+The implementation comes
+from [Microsoft identity platform and the OAuth 2.0 client credentials flow](https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow)
+article.
+
+> The OAuth 2.0 client credentials grant flow permits a web service (confidential client) to use its own credentials,
+> instead of impersonating a user, to authenticate when calling another web service.
+
+## Prerequisites
 
 * Must have administrator access to a Microsoft Azure Active Directory tenant.
 * Must have administrator access to the Admin UI of the Appgate SDP Controller.
-* Must have authtentication provided by SAML for MS Azure AD.
+* Must have authentication provided by SAML for MS Azure AD.
 
-## Azure AD setup
+## Configuration
 
-Follow the [Authentication and authorization basics for Microsoft Graph](https://docs.microsoft.com/en-us/graph/auth/auth-concepts) article via the Application Permissions.  Use the [Register an application with the Microsoft identity platform](https://docs.microsoft.com/en-us/graph/auth-register-app-v2) article to register the Appgate SDP controller.
+| In This Section                                                                                                                                                                                                                                                                                                                  |
+|----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| [Required Information](#required-information)<br>[(1) Retrieve Azure AD tenant and application IDs](#1-retrieve-azure-ad-tenant-and-application-ids)<br>[(2) Prepare script for Appgate use](#2-prepare-script-for-appgate-use)<br>[(3) Create a new user claim script in Appgate](#3-create-a-new-user-claim-script-in-appgate) |
+
+### Required Information
+
+| Information                                 | Format | Purpose                                                                           |
+|---------------------------------------------|--------|-----------------------------------------------------------------------------------|
+| Azure Active Directory Tenant ID            | GUID   | Used in Appgate identity providers to map the Tenant ID attribute to a user claim |
+| Azure Application ID for Appgate            | GUID   | Used to call the Microsoft Graph API to request group names for user claims       |
+| Azure Application client secret for Appgate | text   | Used to call the Microsoft Graph API to request group names for user claims       |       
+
+### (1) Retrieve Azure AD tenant and application IDs
+
+If the below instructions do not align with the current Azure Portal portal interface, please reference the official
+Microsoft links following these instructions.
 
 1. Log into the [Azure Portal](https://portal.azure.com/#home)
 1. Navigate to **Azure Active Directory**
-1. Create or Switch to the AD Tenant (if you are not already in it)
-    1. Copy out the Tenant ID
+1. Create or switch to the correct Active Directory tenant
+    1. Copy the **Tenant ID** for later use
 1. Click on **App Registration**
-    1. Click on **New Registration** if Appgate SDP Controller is not already registered.
-        1. Fill in info, click **Register**
-        1. Select the App from the list
-        1. Copy out the Application (client) ID
-    1. In “Client credentials”, click on **Add a certificate or secret**
-        1. Under “Client Secrets”, click on **New client secret**
-        1. Fill out info, click **Add**
-        1. Find the “Value” of the newly generated secret, copy out the value.
-    1. Click on **API Permissions** in the menu
+    1. Select the Appgate SDP Controller application, if it has already been registered.
+       <br>If there is no registered application for the Appgate SDP Controller, click **New Registration**.
+        1. Fill out the required information, and click **Register** to create the application.
+        1. Select the Appgate SDP Controller application from the list.
+    1. Copy the **Application ID** for later use in the script.
+    1. In **Client credentials**, click **Add a certificate or secret**.
+        1. Within **Client Secrets**, click **New client secret**.
+        1. Fill out the required information, and click **Add**.
+        1. Copy the **Value** of the generated secret for later use.
+    1. In the menu, click **API Permissions**.
         1. Click **Add a permission**
-        1. In the “Request API permissions” modal, select **Microsoft Graph**
-        1. Under “Microsoft Graph”, select **Application Permissions**
-        1. Under Groupmember, select **GroupMember.Read.All**
-        1. Under User, select **User.Read.All**
-        1. Click on **Add permissions**
-        1. Under “Configured permissions”, select **Grant admin consent for 'AppName'**
-        1. Confirm selection
-In “API Permissions”, verify
+        1. In the **Request API permissions** modal, select `Microsoft Graph`.
+            1. Within **Microsoft Graph**, select `Application Permissions`.
+                1. Within **GroupMember**, select `GroupMember.Read.All`.
+                1. Within **User**, select `User.Read.All`
+        1. Click **Add permissions**.
+        1. Within **Configured permissions**, select **Grant admin consent for '{Your_AppName}'**
+            1. Confirm selection.
+        1. In **API Permissions**, verify:
+            - `GroupMember.Read.All` is included
+            - `User.Read.All` is included
+            - Admin consent is granted
 
-## Appgate Controller setup
+**Reference Links**
+
+| Article Link                                                                                                                           | Notes                                                                            |
+|----------------------------------------------------------------------------------------------------------------------------------------|----------------------------------------------------------------------------------|
+| [Authentication and authorization basics (external)](https://learn.microsoft.com/en-us/graph/auth/auth-concepts)                       | Use application permissions (app roles) scenario for Microsoft Graph permissions |
+| [Register an application with the Microsoft identity platform (external)](https://docs.microsoft.com/en-us/graph/auth-register-app-v2) | Create an application for the AppGate SDP Controller                             |
+
+### (2) Prepare script for Appgate use
+
+**Options**
+
+| Variable        | Type      | Line Number | Purpose                                                                                                                                                                                 |
+|-----------------|-----------|-------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| `applicationId` | `string`  | `20`        | The Azure application ID of the registered application created for the Appgate SDP Controller with permissions to call the Microsoft Graph API to retrieve group names for user claims. |
+| `clientSecret`  | `string`  | `33`        | The client secret that corresponds to the `applicationId` with permissions to call the Microsoft Graph API to retrieve group names for user claims.                                     |
+| `logging`       | `boolean` | `42`        | `false`: Do not enable logging for troubleshooting purposes.<br>`true`: Enable logging for troubleshooting purposes.                                                                    |
+
+1. Copy the code from [GraphAPI_Azure.js](./GraphAPI_Azure.js) in this repository.
+2. Replace the value of `applicationId` with the Azure application ID. Ensure this value is wrapped in a single set of
+   quotation marks.
+3. Replace the value of `clientSecret` with the Azure application client secret. Ensure this value is wrapped in a
+   single set of quotation marks.
+4. _Optionally_, temporarily change `logging` to `true` (no quotation marks) to troubleshoot where the script is
+   failing.<br>Ensure this is changed back to `false` once troubleshooting is complete.
+
+---
+
+### (3) Create a new user claim script in Appgate
 
 1. Login to Appgate Controller Admin UI
 1. Navigate to **Scripts** > **User Claims**
-    1. Click **Add New**
-    1. Provide name
-    1. Replace JavaScript with code from [GraphAPI_Azure.js](./GraphAPI_Azure.js) file 
+    1. Click **Add New**.
+    1. Provide a name for the user claims script.
+    1. Replace JavaScript with prepared code
     1. Click **Save**
-1. Navigate to **System** > **Identity Providers** 
-    1. Select IDP that uses the SAML to authenticate against MS Azure AD
+1. Navigate to **System** > **Identity Providers**
+    1. Select the Azure AD SAML identity provider.
     1. Map Attributes to User Claims
-        1. Must have a claim named TenantId mapped with the TenantID
-        1. Must have a claim named UserId mapped with the Object Identitfier
-    1. Add User Claim Script by selecting the name of the script you saved above
-    1. verify
+        1. Must have a claim named `TenantId` mapped to the Azure Tenant ID.
+        1. Must have a claim named `UserId` mapped to the Azure user's object ID or user principal name (UPN).
+    1. Add User Claim Script by selecting the name of the script created above
 1. Navigate to **Operations** > **Policies**
     1. Create or select the Policy for users using MS Azure AD with SAML
-    1. Under “Assignment”, click **Add New**
-        1. Select Identity Provider, with the MS Azure AD using SAML from earlier
-        1. Click on check mark to save
-    1. Under “Assignment”, click **Add New**, again
-        1. To create an assignment criteria that uses a group name from the new list provided, Select **User Claim Script** with an expression like this: “azureGroups.indexOf("INSERT YOUR GROUP NAME HERE") > -1”
-        1. Click on check mark to save
-        1. Click **Save**
+    1. Within **Assignment**, click **Add New**
+        1. Select the SAML MS Azure AD identity provider
+        1. Click check mark to save
+    1. Under **Assignment**, click **Add New**
+        1. Create a group name assignment criteria by selecting **User Claim Script**.
+            1. Create an expression to select the group, replacing `YOUR_GROUP_NAME` with the relevant group
+               name. ```azureGroups.indexOf("YOUR_GROUP_NAME") > -1```
+        1. Click on the check mark to save the assignment.
+    1. Click **Save** to save the policy.
+
+---
